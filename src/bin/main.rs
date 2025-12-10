b067bb7b52d9add4cfb6f98c854f02cd256cb93a
Frans-Willem Hardijzer <fw@hardijzer.nl>
2025-12-10 15:55:46 +0100

    Display working!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,7 +15,16 @@ use esp_hal::gpio::{Level, Output, OutputConfig};
 use esp_hal::gpio::{Input, InputPin, InputConfig, Pull};
 use esp_println::println;
 
+use esp_hal::spi::master::Config as SpiConfig;
+use esp_hal::spi::master::Spi;
+use esp_hal::spi::Mode as SpiMode;
+
+use embedded_hal_bus::spi::ExclusiveDevice;
+use embedded_hal::spi::SpiDevice;
+
 use esp_backtrace as _;
+
+use arrayvec::ArrayVec;
 
 extern crate alloc;
 
@@ -86,10 +95,149 @@ async fn button_task(mut button: Button<'static>, button_name: &'static str) {
     }
 }
 
+#[embassy_executor::task]
+async fn blink_task(mut led: Output<'static>) {
+    loop {
+        //println!("Toggle LED!");
+        led.toggle();
+        Timer::after(Duration::from_millis(500)).await;
+    }
+}
+
+struct Epd<'t, SPIDEV> {
+    busy: Input<'t>,
+    busy_inverted: bool,
+    reset: Output<'t>,
+    dc: Output<'t>,
+    spi_dev: SPIDEV,
+}
+
+impl<'t, SPIDEV> Epd<'t, SPIDEV> {
+    // EPD_W21_Init in example code
+    pub async fn reset(&mut self) {
+       self.reset.set_low();
+       Timer::after(Duration::from_millis(10)).await;
+       self.reset.set_high();
+       Timer::after(Duration::from_millis(10)).await;
+        self.wait_ready().await;
+    }
+
+    pub async fn wait_ready(&mut self) {
+        if self.busy_inverted {
+            self.busy.wait_for_high().await
+        } else {
+            self.busy.wait_for_low().await
+        }
+    }
+}
+impl<'t, SPIDEV> Epd<'t, SPIDEV> where SPIDEV: SpiDevice<u8> {
+    pub fn write(&mut self, cmd: u8, data: &[u8]) -> Result<(), SPIDEV::Error> {
+        self.dc.set_low();
+        self.spi_dev.write(&[cmd])?;
+        if data.len() > 0 {
+            self.dc.set_high();
+            self.spi_dev.write(data)?;
+        }
+        Ok(())
+    }
+
+    pub fn write_iter(&mut self, cmd: u8, data: impl IntoIterator<Item=u8>) -> Result<(), SPIDEV::Error> {
+        self.dc.set_low();
+        self.spi_dev.write(&[cmd])?;
+        let mut buffer = ArrayVec::<u8, 32>::new();
+        for v in data.into_iter() {
+            if buffer.is_full() {
+                self.dc.set_high();
+                self.spi_dev.write(buffer.as_slice())?;
+                buffer.clear();
+            }
+            buffer.push(v);
+        }
+        if !buffer.is_empty() {
+                self.dc.set_high();
+                self.spi_dev.write(buffer.as_slice())?;
+                buffer.clear();
+        }
+        Ok(())
+    }
+
+    pub async fn init(&mut self) -> Result<(), SPIDEV::Error> {
+        self.reset().await;
+
+        self.write(0xAA,&[0x49,0x55,0x20,0x08,0x09,0x18])?;
+        self.write(0x01 /* PWRR */, &[0x3F])?;
+        self.write(0x00 /* PSR */, &[0x5F, 0x69])?;
+        self.write(0x03 /* POFS */, &[0x00, 0x54, 0x00, 0x44])?;
+        self.write(0x05 /* BTST1 */, &[0x40, 0x1F, 0x1F, 0x2C])?;
+        self.write(0x06 /* BTST2 */, &[0x6F, 0x1F, 0x17, 0x49])?;
+        self.write(0x08 /* BTST3 */, &[0x6F, 0x1F, 0x1F, 0x22])?;
+        self.write(0x30 /* PLL */, &[0x03])?; // esphome does 0x03, example code for 0x08
+        self.write(0x50 /* CDI */, &[0x3F])?;
+        self.write(0x60 /* TCON */, &[0x02, 0x00])?;
+        self.write(0x61 /* TRES */, &[0x03, 0x20, 0x01, 0xE0])?;
+        self.write(0x84 /* T_VDCS*/, &[0x01])?;
+        self.write(0xE3 /* PWS */, &[0x2F])?;
+        self.write(0x04 /* PWR on */, &[])?;
+
+        self.wait_ready().await;
+        Ok(())
+    }
+
+    pub async fn display(&mut self, data: impl IntoIterator<Item=Spectra6Color>) -> Result<(), SPIDEV::Error> {
+        self.write_iter(0x10, SpectraPacker(data.into_iter()))?;
+        self.write(0x12, &[0x00])?;
+        Timer::after(Duration::from_millis(20)).await;
+        self.wait_ready().await;
+        Ok(())
+    }
+
+    pub async fn sleep(&mut self) -> Result<(), SPIDEV::Error> {
+        self.write(0x02, &[0x00])?;
+        self.wait_ready().await;
+        Ok(())
+    }
+}
+
+enum Spectra6Color {
+    Black = 0,
+    White = 1,
+    Yellow = 2,
+    Red = 3,
+    Blue = 5,
+    Green = 6,
+    Clean = 7,
+}
+
+struct SpectraPacker<T>(T);
+
+impl<T> Iterator for SpectraPacker<T> where T: Iterator<Item=Spectra6Color> {
+    type Item = u8;
+    fn next(&mut self) -> Option<Self::Item> {
+        let left = self.0.next()?;
+        let right = self.0.next().unwrap_or(Spectra6Color::White);
+        Some((left as u8) << 4 | (right as u8))
+    }
+}
+
+fn test_screen(width: usize, height: usize) -> impl Iterator<Item=Spectra6Color> {
+    (0 .. width * height).map(move |index| {
+        let _x = index % width;
+        let y = index / width;
+        match (y / 32) % 6 {
+            0 => Spectra6Color::White,
+            1 => Spectra6Color::Black,
+            2 => Spectra6Color::Red,
+            3 => Spectra6Color::Green,
+            4 => Spectra6Color::Blue,
+            5 => Spectra6Color::Yellow,
+            _ => Spectra6Color::White,
+        }
+    })
+}
+
 #[esp_rtos::main]
 async fn main(spawner: Spawner) -> ! {
     // generator version: 1.0.1
-
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
@@ -103,20 +251,50 @@ async fn main(spawner: Spawner) -> ! {
         esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
             .expect("Failed to initialize Wi-Fi controller");
 
-    let mut led = Output::new(peripherals.GPIO6, Level::High, OutputConfig::default());
+    //let mut led = Output::new(peripherals.GPIO6, Level::High, OutputConfig::default());
 
     spawner.spawn(button_task(Button::new(peripherals.GPIO3, InputConfig::default().with_pull(Pull::Up), true), "Refresh")).unwrap();
     spawner.spawn(button_task(Button::new(peripherals.GPIO4, InputConfig::default().with_pull(Pull::Up), true), "Right")).unwrap();
     spawner.spawn(button_task(Button::new(peripherals.GPIO5, InputConfig::default().with_pull(Pull::Up), true), "Left")).unwrap();
+    spawner.spawn(blink_task(Output::new(peripherals.GPIO6, Level::Low, OutputConfig::default()))).unwrap();
+
+    let epd_spi_bus = Spi::new(
+        peripherals.SPI2,
+        SpiConfig::default()
+        .with_write_bit_order(esp_hal::spi::BitOrder::MsbFirst)
+        .with_frequency(esp_hal::time::Rate::from_mhz(20))
+        .with_mode(SpiMode::_0),
+    ).unwrap();
+    let epd_spi_bus = epd_spi_bus
+        .with_sck(peripherals.GPIO7)
+        .with_mosi(peripherals.GPIO9);
+
+    let epd_spi_dev = ExclusiveDevice::new(epd_spi_bus, Output::new(peripherals.GPIO20, Level::Low, OutputConfig::default()), esp_hal::delay::Delay::new()).unwrap();
+
+    let mut epd = Epd {
+        busy: Input::new(peripherals.GPIO13, InputConfig::default().with_pull(Pull::Up)),
+        busy_inverted: true,
+        reset: Output::new(peripherals.GPIO12, Level::Low, OutputConfig::default()),
+        dc: Output::new(peripherals.GPIO11, Level::Low, OutputConfig::default()),
+        spi_dev: epd_spi_dev,
+    };
+
+    println!("init");
+    epd.init().await.unwrap();
+    println!("display");
+    epd.display(test_screen(800,480)).await.unwrap();
+    println!("sleep");
+    epd.sleep().await.unwrap();
+    println!("Done");
+
+      
+
 
     // TODO: Spawn some tasks
     let _ = spawner;
 
     loop {
-        println!("Toggle me!");
-        led.toggle();
-        Timer::after(Duration::from_millis(500)).await;
+        Timer::after(Duration::from_secs(1)).await;
     }
-
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
 }
